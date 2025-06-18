49993472d7f2437209757cb94ff79d0e5835ba0c
fardeen-mohd-rafiq <fardeen2903@gmail.com>
2025-06-18 16:50:14 +0530

    refactor: Comment out unused operator categories for better code clarity

--- src/pages/operatorsData.js
+++ src/pages/operatorsData.js
@@ -12,33 +12,33 @@ export const masterOperators = [
         { id: "6", name: "Does not contain", value: "does_not_contain" },
       ],
     },
-    {
-      id: "1",
-      name: "Logical Operatives",
-      subOptions: [
-        { id: "1", name: "AND", value: "and" },
-        { id: "2", name: "OR", value: "or" },
-        { id: "3", name: "NOT", value: "not" },
-      ],
-    },
-    {
-      id: "2",
-      name: "Date/Time Operatives",
-      subOptions: [
-        { id: "1", name: "Before", value: "before" },
-        { id: "2", name: "After", value: "after" },
-        { id: "3", name: "Between", value: "between" },
-        { id: "4", name: "Within", value: "within" },
-      ],
-    },
-    {
-      id: "3",
-      name: "Tier Operatives",
-      subOptions: [
-        { id: "1", name: "Is in tier", value: "is_in_tier" },
-        { id: "2", name: "Upgrade", value: "upgrade" },
-        { id: "3", name: "Downgrade", value: "downgrade" },
-      ],
-    },
+    // {
+    //   id: "1",
+    //   name: "Logical Operatives",
+    //   subOptions: [
+    //     { id: "1", name: "AND", value: "and" },
+    //     { id: "2", name: "OR", value: "or" },
+    //     { id: "3", name: "NOT", value: "not" },
+    //   ],
+    // },
+    // {
+    //   id: "2",
+    //   name: "Date/Time Operatives",
+    //   subOptions: [
+    //     { id: "1", name: "Before", value: "before" },
+    //     { id: "2", name: "After", value: "after" },
+    //     { id: "3", name: "Between", value: "between" },
+    //     { id: "4", name: "Within", value: "within" },
+    //   ],
+    // },
+    // {
+    //   id: "3",
+    //   name: "Tier Operatives",
+    //   subOptions: [
+    //     { id: "1", name: "Is in tier", value: "is_in_tier" },
+    //     { id: "2", name: "Upgrade", value: "upgrade" },
+    //     { id: "3", name: "Downgrade", value: "downgrade" },
+    //   ],
+    // },
   ];
   
