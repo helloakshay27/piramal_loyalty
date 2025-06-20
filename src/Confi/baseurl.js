bdba766470d63fc10ebafe833edfac5fa83a7d6c
fardeen-mohd-rafiq <fardeen2903@gmail.com>
2025-06-20 19:50:09 +0530

    refactor: Update BASE_URL assignment logic to dynamically set based on hostname

--- src/Confi/baseurl.js
+++ src/Confi/baseurl.js
@@ -1,2 +1,11 @@
-const BASE_URL = "https://piramal-loyalty-dev.lockated.com";
+const BASE_URL = ""
+
+const hostname = window.location.hostname;
+
+if( hostname === "https://uat-loyalty.lockated.com/") {
+    BASE_URL = "https://uat-loyalty.lockated.com/"
+} else {
+    BASE_URL = "https://piramal-loyalty-dev.lockated.com";
+}
+
 export default BASE_URL;
