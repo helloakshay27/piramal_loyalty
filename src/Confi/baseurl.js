5d8ae56716673ef887a0d7784932c4528e64f6d8
fardeen-mohd-rafiq <fardeen2903@gmail.com>
2025-06-21 12:03:03 +0530

    refactor: Fix hostname comparison in BASE_URL assignment logic

--- src/Confi/baseurl.js
+++ src/Confi/baseurl.js
@@ -3,7 +3,7 @@ let BASE_URL = ""
 const hostname = window.location.hostname;
 console.log("Current hostname:", hostname);
 
-if( hostname === "https://uat-loyalty.lockated.com/") {
+if( hostname === "uat-loyalty.lockated.com") {
     BASE_URL = "https://uat-loyalty.lockated.com/"
 } else {
     BASE_URL = "https://piramal-loyalty-dev.lockated.com";
