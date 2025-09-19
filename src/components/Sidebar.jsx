0c89e3a0a8876bfa288945d99325158f0d70b884
fardeen-mohd-rafiq <fardeen2903@gmail.com>
2025-09-19 12:59:50 +0530

    feat: Remove conditional rendering of sidebar items for rustomjee-loyalty domain

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -209,61 +209,6 @@ const Sidebar = () => {
 
           </NavLink>
         </li>
-        {domain === "rustomjee-loyalty.lockated.com" && (
-
-          <li className="nav-item">
-            <a
-              className="text nav-link d-flex justify-content-between"
-              data-bs-toggle="collapse"
-              data-bs-target="#userRoleCollapse"
-            >
-              <span className="text">Engage</span>
-              {isCollapsed ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  className="bi bi-caret-down-fill"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  className="bi bi-caret-right-fill"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
-                </svg>
-              )}
-            </a>
-            <ul className="collapse p-0" id="userRoleCollapse">
-              <li>
-                <NavLink
-                  className={`ps-4 ${activeItem === "/Segment" ? "active" : ""}`}
-                  to="/Segment"
-                  onClick={() => handleNavLinkClick("/Segment")}
-                >
-                  Segment
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  className={`ps-4 ${activeItem === "/Campaign" ? "active" : ""}`}
-                  to="/Campaign"
-                  onClick={() => handleNavLinkClick("/Campaign")}
-                >
-                  Campaign
-                </NavLink>
-              </li>
-            </ul>
-          </li>
-        )}
 
         {domain === "rustomjee-loyalty.lockated.com" && (
           <>
